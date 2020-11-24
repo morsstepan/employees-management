c9c294dd82eb2d065f2505c1e1e9e508b4901d0e
Stepan Morozov <morsstepan@icloud.com>
2020-11-24 18:59:52 +0300

    Add Spring Boot scheme

---
+++ src/test/java/om/github/morsstepan/ysu/moderntechnologies/employees/employeesmanagement/EmployeesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package om.github.morsstepan.ysu.moderntechnologies.employees.employeesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
